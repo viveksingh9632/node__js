3199ae56052c713d710ff4115befc8b603355701
vivek <viveksinghranghar>
2024-03-27 19:28:13 +0530

    Commit message

--- file.js
+++ file.js
@@ -1,14 +1,31 @@
 const fs=require("fs")
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    for (let i = 0; i < 5; i++) {
+        console.log(`Waiting ${i} seconds...`);
+        await sleep(i * 1000);
+    }
+    console.log('Done');
+}
+
+
+console.log(1)
 //sync.....
-fs.writeFileSync('./test.text',"Hello world")
+// fs.writeFileSync('./test.text',"With Sync")
 
+// const result=fs.readFileSync("./test.text","utf-8")
 
+// console.log(result)
 
+console.log(2)
 
 //Async....
 
-fs.writeFile('./test.text', 'Hello Nandu', (err) => {
+fs.writeFile('./test.text', 'Without Sync', (err) => {
     if (err) {
         console.error('An error occurred:', err);
     } else {
@@ -17,16 +34,29 @@ fs.writeFile('./test.text', 'Hello Nandu', (err) => {
 });
 
 
-
-
-const result=fs.readFileSync("./contacts.text","utf-8")
-console.log(result)
-
-
-fs.readFile("./contacts.text","utf-8",(err,result)=>{
+// const result=fs.readFileSync("./test.text","utf-8")
+// console.log(result)
+var a = "sadf";
+fs.readFileSync('./test.text', 'utf8', (err, data) => {
     if (err) {
-        console.log("Error",err)
-    }else{
-        console.log(result)
+      console.error(err);
+      return;
     }
-})
+    var a = data
+    console.log(data);
+  });
+
+  console.log(a)
+
+console.log(3)
+
+
+
+
+// fs.readFile("./contacts.text","utf-8",(err,result)=>{
+//     if (err) {
+//         console.log("Error",err)
+//     }else{
+//         console.log(result)
+//     }
+// })
